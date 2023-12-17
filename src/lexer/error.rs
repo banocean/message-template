5ca76ffd83031d216a804a51942d7efb50756455
banocean <47253870+banocean@users.noreply.github.com>
2023-12-17 16:17:16 +0100

    tokenization error struct

---
+++ src/lexer/error.rs
@@ -0,0 +1,46 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct TokenizationError {
+    kind: TokenizationErrorKind,
+    backtrace: Backtrace
+}
+
+#[derive(Debug)]
+pub enum TokenizationErrorKind {
+    InvalidChar(char),
+
+}
+
+#[derive(Debug)]
+pub struct Backtrace {
+    position: usize
+}
+
+impl TokenizationError {
+    pub fn new(kind: TokenizationErrorKind, position: usize) -> Self {
+        Self {
+            kind,
+            backtrace: Backtrace { position },
+        }
+    }
+
+    pub fn kind(&self) -> &TokenizationErrorKind {
+        &self.kind
+    }
+}
+
+impl fmt::Display for TokenizationError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.description().fmt(f)
+    }
+}
+
+impl Error for TokenizationError {
+    fn description(&self) -> &str {
+        match self.kind {
+            TokenizationErrorKind::InvalidChar(char) => "Invalid character in code block"
+        }
+    }
+}
